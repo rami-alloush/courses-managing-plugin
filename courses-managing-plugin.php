ab18676b03d6f7bfbd9e76cfb168049800d80e37
MrNoComment <Mr.NoComment@MSN.com>
2015-02-11 04:26:21 +0300

    Update courses-managing-plugin.php

--- courses-managing-plugin.php
+++ courses-managing-plugin.php
@@ -17,7 +17,7 @@ Issues to be addressed:
 ******************************************************************************
 
 Change Log:
-3.8.7:	Show Me Notification
+3.8.7:	Show Me Notification PLZ
 3.8.6:	Updater skip bad release
 3.8.5:	Work on updater reactivation
 3.8.4:	Work on updater info
